b0c27f3cc704552ae167d40c9ee741fa0ee4784c
Patrick Waivers <pwaivers@gmail.com>
2013-06-27 18:26:55 -0400

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # This is code from the Ruby on Rails tutorial onlune in Listing 3.10
+  # http://ruby.railstutorial.org/chapters/static-pages?version=4.0#top
+  config.include Capybara::DSL
 end
